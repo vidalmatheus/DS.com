79ad5ee5f2a65d9aae911ba8b4efe2227a52f51e
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-06-06 00:38:15 -0300

    make it better

--- app.py
+++ app.py
@@ -1,51 +1,46 @@
 from flask import Flask, render_template, request, redirect, json, url_for
-import psycopg2
+import psycopg2, subprocess
+from config import *
 
 # create app
 app = Flask(__name__,static_url_path='/static')
 
-### connect to the db 
-con = psycopg2.connect(
-            host = "ec2-50-19-114-27.compute-1.amazonaws.com",
-            database="d4hj7938me3577",
-            user = "pjiswamglyhfme",
-            port = 5432,
-            password = "2829cb6cc89be5cfbe36da3706dfe468c9b8a46c2f2560f85ac05e5ec7870b80")
-####
-
+# main page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# login page
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-@app.route('/cadastro', methods=['GET', 'POST'])
-def cadastro():
+# 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
         # Fetch form data
         userDetails = request.form
         cpf = userDetails['cpf']
-        senha = userDetails['senha']
+        psd = userDetails['psd']
         saram = userDetails['saram']
-        nome = userDetails['nome']
-        dt_nasc = userDetails['dt_nasc']
-        sexo = userDetails['sexo']
-        endereco = userDetails['endereco']
-        tel = userDetails['telefone']
+        name = userDetails['name']
+        birth_date = userDetails['birth_date']
+        sex = userDetails['sex']
+        adress = userDetails['adress']
+        phone = userDetails['phone']
         email = userDetails['email']
-        militar = userDetails['militar']
+        military = userDetails['military']
         #cursor 
         cur = con.cursor()  
-        print(cpf,senha,saram,nome,dt_nasc,sexo,endereco,tel,email,militar)
-        cur.execute("INSERT INTO paciente VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(cpf,senha,saram,nome,dt_nasc,sexo,endereco,tel,email,militar,False))
+        print(cpf,psd,saram,name,birth_date,sex,adress,phone,email,military)
+        cur.execute("INSERT INTO paciente VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(cpf,psd,saram,name,birth_date,sex,adress,phone,email,military,False))
         #commit the transcation 
         con.commit()
         #close the cursor
         cur.close()
         return redirect('/users')
-    return render_template('cadastro.html')
+    return render_template('register.html')
 
 @app.route('/users')
 def users():
@@ -53,18 +48,6 @@ def users():
     resultValue = cur.execute("SELECT * FROM paciente")
     userDetails = cur.fetchall()
     return render_template('users.html',userDetails=userDetails)
-
-'''
-#--- incio: teste externo
-cur.execute("select CPF, nome from paciente")
-rows = cur.fetchall()
-
-print ("CPF             Nome")
-for r in rows:
-    print (f" {r[0]} | {r[1]} ")
-
-#--- fim: teste externo
-'''
 
 if __name__ == '__main__':
     app.run(debug=True)
