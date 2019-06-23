c9b2b6017cdb99ae41f04430b4445fefb633aeac
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-06-23 20:45:40 -0300

    login

--- routes/changeRegisterRoute.py
+++ routes/changeRegisterRoute.py
@@ -5,6 +5,7 @@ changeRegister_api = Blueprint('changeRegister_api', __name__)
 # users registers
 @changeRegister_api.route('/changeregister', methods=['GET', 'POST'])
 def changeRegister():
+    global usersDataOnline
     print("////////////////////////////////////////")
     print("comeca change register")
     print("usersDataOnline.getDictionary() = "+ str(usersDataOnline.getDictionary()))
--- routes/logoutRoute.py
+++ routes/logoutRoute.py
@@ -3,9 +3,13 @@ logout_api = Blueprint('logout_api', __name__)
 
 @logout_api.route('/logout')
 def logout():
+    global usersDataOnline
     #userData.logOutUser()
     print("/////////////////////////////")
     print("Começa logout")
+    if not 'user' in session:
+        print("not 'user' so return to '/'")
+        return redirect('/') 
     usersDataOnline.logoutUser(session['user'])
     session.pop('user', None)
     print("usersDataOnline.dictUsersOn = " + str(usersDataOnline.dictUsersOn))
