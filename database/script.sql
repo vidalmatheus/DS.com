28bcda2a23d08a8f37062051bddacf04a92e70c1
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-06-26 17:31:59 -0300

    script.sql

--- database/script.sql
+++ database/script.sql
@@ -16,6 +16,7 @@ CREATE DATABASE ds
 DROP TABLE IF EXISTS Medico CASCADE;
 DROP TABLE IF EXISTS Paciente CASCADE;
 DROP TABLE IF EXISTS Consulta CASCADE;
+DROP TABLE IF EXISTS Logado CASCADE;
 
 -- ----------------------------------------------------- 
 -- Table `ds`.`medico`
@@ -23,6 +24,7 @@ DROP TABLE IF EXISTS Consulta CASCADE;
 DROP TABLE IF EXISTS "ds".medico ;
 
 CREATE TABLE Medico (
+	CPF varchar(14) unique NOT NULL,
     CRM int unique NOT NULL,
 	senha varchar NOT NULL,
 	SARAM int unique,
@@ -56,6 +58,8 @@ CREATE TABLE Paciente (
 -- -----------------------------------------------------
 -- Table `ds`.`consulta`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS "ds".consulta ;
+
 CREATE TABLE Consulta (
     id SERIAL,
     CPF_pac varchar(14) unique NOT NULL,
@@ -66,7 +70,19 @@ CREATE TABLE Consulta (
     PRIMARY KEY (id)
 );
 
+-- -----------------------------------------------------
+-- Table `ds`.`logado`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS "ds".logado ;
 
+CREATE TABLE Logado (
+    CPF varchar(14) unique NOT NULL,
+	session_hash varchar(60) unique NOT NULL, 
+    PRIMARY KEY (CPF)
+);
+
+--ALTER TABLE Logado ADD FOREIGN KEY (CPF) REFERENCES Paciente(CPF);
+--ALTER TABLE Logado ADD FOREIGN KEY (CPF) REFERENCES Medico(CPF);
 ALTER TABLE Consulta ADD FOREIGN KEY (CRM) REFERENCES Medico(CRM);
 ALTER TABLE Consulta ADD FOREIGN KEY (CPF_pac) REFERENCES Paciente(CPF);
 
@@ -84,20 +100,24 @@ insert into Paciente values
 (04339241698,'123456',9654321,'Pedro Alves','1997/04/04','M','Rua H8B,203 - SJC,SP','(18)99605-7649','alvesouza@gmail.com','alunoITA',false);
 
 insert into Medico values 
-(16863158,'123456',1234567,'Charlie','odontologista','1tenente'),
-(15846758,'123456',1234568,'Jonas','ortopedista','2tenente'),
-(15523418,'123456',1234569,'Nathalia','oftalmologista','capitao');
+(13420296741,16863158,'123456',1234567,'Charlie','odontologista','1tenente'),
+(13420296742,15846758,'123456',1234568,'Jonas','ortopedista','2tenente'),
+(13420296743,15523418,'123456',1234569,'Nathalia','oftalmologista','capitao');
 
 insert into Consulta (CPF_pac,CRM,Data,Hora,status) values 
 (13420296746,16863158,'2019/05/27','11:00:00','aberto'),
 (43967557839,15846758,'2019/05/28','15:30:00','marcado');
+
+insert into Logado values 
+(13420296746,'$2b$12$9hihmAkRabPNb5MhIk0ui.mY7rEjDGdcctGaRR2/DHflaa9ldDAAA'),
+(13420296741,'$2b$12$9hihmAkRabPNb5MhIk0ui.mY7rEjDGdcctGaRR2/DHflaa9ldDBBB');
 */
 
 -- -----------------------------------------------------
 -- Query
 -- -----------------------------------------------------
 
-select * from Paciente;
+--select * from Paciente;
 --select * from medico;
 --select * from Consulta;
 
@@ -109,6 +129,17 @@ from
 where status = 'marcado'
 */
 
+/*
+-- Retorna pacientes logados
+select * from
+(select * from paciente) as p inner join (select * from logado) as l on (p.CPF=l.CPF)
+*/
+
+/*
+-- Retorna medicos logados
+select * from
+(select * from medico) as m inner join (select * from logado) as l on (m.CPF=l.CPF)
+*/
 
 
 
