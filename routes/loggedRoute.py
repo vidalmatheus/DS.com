2c49cb38b0a483b22ad25e713646c06a7de3abe2
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-06-27 08:59:52 -0300

    corrigido erro de login paciente

--- routes/loggedRoute.py
+++ routes/loggedRoute.py
@@ -46,16 +46,6 @@ def logged():
 
     print(request.args.get('userDetails'))
     #userData = usersDataOnline.getUser(session['user'])
-    if not "verifica no banco de dados se esta logado" == "verifica no banco de dados se esta logado":
-        print("SE TIVER EM SESSION MAS NAO ESTA NO BANCO DE LOGADOS")
-
-
-        session.pop("loginHash", None)
-        session.pop("userName", None)
-        session.pop("userCPF", None)
-        session.pop("userType", None)
-
-        return redirect('/login')
 
     print("RENDERIZA A TELA DE LOGGED")
 
